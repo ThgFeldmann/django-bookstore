e23839bbbf3d0925c6120e17cc034df4026b72dd
Thiago <thiagofeldmann06@gmail.com>
2025-02-20 13:49:38 -0300

    fix: authentication
    fixed:
    - authentication not working properly
    - order-viewset missing arguments

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
@@ -7,8 +7,9 @@ from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
-
     serializer_class = OrderSerializer
-    queryset = Order.objects.all().order_by("id")
+
+    def get_queryset(self):
+        return Order.objects.all().order_by("id")
